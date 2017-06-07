2495a96a80445180c3cadcf1c975bc809794bec1
Nickhil Sethi <nickhilsethi@Nickhils-MacBook-Pro.local>
2017-06-07 10:47:24 -0400

    check_validity()

--- group_students.js
+++ group_students.js
@@ -55,5 +55,80 @@ function generate_groupings(num_students,num_groups){
 	}
 	return groups
 }
+function is_noisy(idx, students){
+	if student[idx]['noisy']{
+		return 1
+	} else {
+		return 0
+	};
+}
 
+function add(a,b){
+	return a + b;
+}
+
+function check_validity(grouping,students,num_groups){
+	/* Checks validity of one particular grouping of students against traits in students_dict.
+
+	Parameters
+	__________ 
+
+	grouping : type list[int]
+		grouping of len(grouping) students into groups
+
+	students : type list[dict]
+		list of dictionary (unpacked from json) of students with traits
+
+	Returns
+	_______ 
+
+	valid : type bool 
+		True if grouping is valid according to requirements. */
+
+	for (group_index=0; group_index < num_groups; group_index++){
+		
+		// extract student indices for students in group
+		var group = []
+		for (student_idx = 0; student_idx < grouping.length; student_index++){
+			if (grouping[student_idx] == group_index){
+				group.push(student_idx);
+			}
+		}
+
+		// assert no more than 2 students are noisy
+		var num_noisy = 0
+		for (i = 0; i < group.length; i++){
+			if (students[i]['noisy']){
+				num_noisy++
+			}
+		}
+		if (num_noisy > 2){
+			return false
+		}
+		
+		// assert at least one student understands the material
+		var num_understand = 0
+		for (i=0 ; i < group.length; i++){
+			if (students[i]['understands']){
+				num_understand++
+			}
+		}
+		if (num_understand < 1){
+			return false
+		}
+
+		// check if students in group fight
+		for (i=0; i < group.length; i++) {
+			var student1 = group[i]
+			for (j=i+1; j < group.length; j++) {
+				var student2 = group[j]
+				if (students[student1]['name'] in students[student2]['fights_with']){
+					return false
+				}
+			}
+		}
+
+	}
+	return true
+}
 console.log(generate_groupings(4,2))
